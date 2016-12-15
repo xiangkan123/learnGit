9ffa99cbc43800dfca46806ee89439f37ab1970d
hongtian <qq786047706@163.com>
2016-12-15 11:14:41 +0800

    change test data

--- test.lua
+++ test.lua
@@ -1,9 +1,8 @@
 local a = 15036
 
 local function get_num(num)
-	num = num / 10
-	if num == 0 then
-		return 0
+	while num ~= 0 do
+		num = math.floor(num / 10)
+		temp = num % 10
 	end
-	return num % 10
 end
